509ce5186ad77d3f2c0998cb1eb4964769f40076
Denys Perevertun <dperik228@gmail.com>
2021-10-04 12:10:29 +0300

    Funkcje

---
+++ battleship.py
@@ -0,0 +1,38 @@
+def generation_board():
+    pass
+
+
+def print_board():
+    pass
+
+
+def ai_player():
+    pass
+
+
+def player():
+    pass
+
+
+def mark():
+    pass
+
+
+def clear():
+    pass
+
+
+def win():
+    pass
+
+
+def validate():
+    pass
+
+
+def shooting_phase():
+    pass
+
+
+def battleship():
+    pass
