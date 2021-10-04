b78423c557b14b567a05e6619197edff8c7fa66b
Sebastian Ryndak <ryndaksebastiann@gmail.com>
2021-10-04 13:39:52 +0200

    print_board

--- battleship.py
+++ battleship.py
@@ -5,11 +5,29 @@ def generation_board(height,width):
         board.append(['.'] * width)
     return board
 
-def print_board(board):
+def print_board5_5(board):
+    a = 0
+    print('        A B C D E ')
+    print('        v v v v v ')
+    print('       ___________')
     for element in board:
+        a += 1
+        b = str(a)
         element = ' '.join(element)
-        print(element)
-        
+        print(b.rjust(2),"=>","|", element,"|")
+    print('       -----------') 
+
+def print_board10_10(board):
+    a = 0
+    print('        A B C D E F G H I J')
+    print('        v v v v v v v v v v')
+    print('       ____________________')
+    for element in board:
+        a += 1
+        b = str(a)
+        element = ' '.join(element)
+        print(b.rjust(2),"=>","|", element,"|")
+    print('       --------------------') 
 
 
 def ai_player():  # Denys
@@ -43,9 +61,23 @@ def shooting_phase():  # Vładek
 def battleship():
     pass
 
+def size_of_the_board():
+    size = input('''    1 = Board size 5x5
+    2 = Board size 10x10
+    Your choice: ''')
+    if size == '1':
+        board = generation_board(5,5)
+        print_board5_5(board)
+    elif size == '2':
+        board = generation_board(10,10)
+        print_board10_10(board)
+    else:
+        print('Choose one of the following options!!!')
+        size_of_the_board()
 def main_menu():
-    pass
+    size_of_the_board()
+    
 
 if __name__ == "__main__":
-    board = generation_board(5,5)
-    print_board(board)
+    main_menu()
+    
