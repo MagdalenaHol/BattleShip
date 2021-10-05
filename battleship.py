98a6d0bbd1ecf101280639e1f9675387b8db0183
Denys Perevertun <dperik228@gmail.com>
2021-10-05 10:26:14 +0300

    Move from player, or Ai_player

--- battleship.py
+++ battleship.py
@@ -1,10 +1,13 @@
 import os
+import random
 
-def generation_board(height, width): 
+
+def generation_board(height, width):
     board = []
     for i in range(height):
         board.append(['.'] * width)
     return board
+
 
 def print_board5_5(board):
     char = 0
@@ -14,9 +17,10 @@ def print_board5_5(board):
     for element in board:
         char += 1
         element = ' '.join(element)
-        print(chr(char + 64).rjust(2),"=>","|", element,"|")
-    print('       -----------') 
+        print(chr(char + 64).rjust(2), "=>", "|", element, "|")
+    print('       -----------')
     return board
+
 
 def print_board10_10(board):
     char = 0
@@ -26,16 +30,32 @@ def print_board10_10(board):
     for element in board:
         char += 1
         element = ' '.join(element)
-        print(chr(char + 64).rjust(2),"=>","|", element,"|")
-    print('       --------------------') 
+        print(chr(char + 64).rjust(2), "=>", "|", element, "|")
+    print('       --------------------')
 
 
-def ai_player():  # Denys
-    pass
+coordinates_dict = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
 
 
-def player():  # Denys
-    pass
+def ai_player(coordinates):
+    bot_move = random.choice(coordinates)
+    coordinates.remove(bot_move)
+    row = int(coordinates_dict[bot_move[0]])
+    col = int(bot_move[1])
+    return row, col-1
+
+
+def player(used_coordinates, coordinates):
+    user_input = validate()
+    row = int(coordinates_dict[user_input[0]])
+    col = int(user_input[1])
+    return row, col-1
+
+
+def quit_game(user_input):
+    if user_input.upper() == "QUIT":
+        print("Goodbye")
+        exit()
 
 
 def mark():  # Magda
@@ -45,6 +65,7 @@ def mark():  # Magda
 def clear():  # NIKT
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
 def win():  # Magda
     pass
 
@@ -53,15 +74,17 @@ def validate(board):  # Vładek
     pass
 
 
-def set_of_coordinates(row_selected, col_selected): 
-    options_to_choose = {"A": 0, "B": 1, "C": 2,"D ": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9,
-     "1": 0, "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "10": 9}
+def set_of_coordinates(row_selected, col_selected):
+    options_to_choose = {"A": 0, "B": 1, "C": 2, "D ": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9,
+                         "1": 0, "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "10": 9}
     row = options_to_choose[row_selected]
     col = options_to_choose[col_selected]
     return row, col
 
+
 def select_coordinates(board, row, col):
-    cell_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    cell_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I",
+                 "J", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     row_selected = input("Select a cell ").upper()
     col_selected = input("Select a cell ")
     if row_selected and col_selected in cell_list:
@@ -70,9 +93,10 @@ def select_coordinates(board, row, col):
         print("Invalid input! You must select a cell on the board")
         select_coordinates(board, row, col)
 
-def shooting_phase(board, row, col,height,width):  # Vładek
-    board_for_note_player_1 = generation_board(height,width)
-    board_for_note_player_2 = generation_board(height,width)
+
+def shooting_phase(board, row, col, height, width):  # Vładek
+    board_for_note_player_1 = generation_board(height, width)
+    board_for_note_player_2 = generation_board(height, width)
     select_coordinates(board, row, col)
     if board_player_1[row][col] == '.':
         board_for_note_player_1[row][col] == 'M'
@@ -82,7 +106,7 @@ def shooting_phase(board, row, col,height,width):  # Vładek
         board_for_note_player_1[row][col] == 'H'
         print("You've hit a ship!")
         print_board(board_for_note_player_1)
-    if  board_player_2[row][col] == '.':
+    if board_player_2[row][col] == '.':
         board_for_note_player_1[row][col] == 'M'
         print("You've missed!")
         print_board(board_for_note_player_2)
@@ -90,14 +114,11 @@ def shooting_phase(board, row, col,height,width):  # Vładek
         board_for_note_player_1[row][col] == 'H'
         print("You've hit a ship!")
         print_board(board_for_note_player_2)
-        
 
-
-
-    
 
 def battleship():
     pass
+
 
 def size_of_the_board():
     print('    Select board size:\n')
@@ -106,18 +127,19 @@ def size_of_the_board():
     Your choice: ''')
     clear()
     if size == '1':
-        board = generation_board(5,5)
+        board = generation_board(5, 5)
         print_board5_5(board)
     elif size == '2':
-        board = generation_board(10,10)
+        board = generation_board(10, 10)
         print_board10_10(board)
     else:
         print('Choose one of the following options!!!')
         size_of_the_board()
+
+
 def main_menu():
     size_of_the_board()
-    
+
 
 if __name__ == "__main__":
     main_menu()
-    
