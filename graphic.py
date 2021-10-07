5042c7a8a0a315ed28cd7b3b7d302b51d4610c70
Sebastian Ryndak <ryndaksebastiann@gmail.com>
2021-10-07 17:29:24 +0200

    grafika

---
+++ graphic.py
@@ -0,0 +1,18 @@
+import time
+
+
+ship=['                                     # #  ( )',
+'                                  ___#_#___|__       ',
+'                              _  |____________|  _   ',
+'                       _=====| | |            | | |==== _',
+'                 =====| |.---------------------------. | |====',
+'   <--------------------   .  .  .  .  .  .  .  .     --------------/',
+'     \                                                             /',
+'      \_______________________________________________WWS_________/',
+'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww',
+'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww',
+'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww']
+
+for i in ship:
+    print(i)
+    time.sleep(0.1)
